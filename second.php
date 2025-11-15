fb0f8843fb1d52ec63a8eeeda1e12d80e2b5552c
a97404203-hub <a97404203@gmail.com>
2025-11-15 10:40:28 +0530

    Add files via upload

---
+++ second.php
@@ -0,0 +1,22 @@
+<?php
+$unit=120;
+if($unite<50)
+
+    {
+    
+    echo"bill=.$unit*3;
+}
+    else if($unite<150)
+{
+    echo"bill=".unites*5;
+}
+   else if ($unite>150)
+}
+   else if($unite>150)
+   {
+     echo"bill=".$unite*8;
+
+}
+
+
+    ?>
